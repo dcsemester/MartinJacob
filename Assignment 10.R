278438a62bb99167c87e4a3120b2cc69a397262a
Martin Jacob <martin.jacob@ufl.edu>
2018-10-25 14:33:33 -0400

    rename file to numbered file

--- Assignment 10.R
+++ Assignment 10.R
@@ -15,3 +15,5 @@ fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
 fish_data_cat <- fish_data %>% 
   mutate(length_cat = ifelse(length > 300, "big", "small"))%>% 
   filter(scalelength > 1)
+
+
