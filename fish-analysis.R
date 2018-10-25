dbafbc8218fba8da3217ace2969e6c36442e55d2
Martin Jacob <martin.jacob@ufl.edu>
2018-10-25 14:34:58 -0400

    rename file to numbered file

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,7 +1,8 @@
 
-
-fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
+file.rename(from = "data/Gaeta_etal_CLC_data.csv", to = "data/Gaeta_etal_CLC_data_1.csv")
+fish_data <- read.csv("data/Gaeta_etal_CLC_data1.csv")
 
 library(dplyr)
 fish_data_cat <- fish_data %>% 
-  mutate(length_cat = ifelse(length > 300, "big", "small"))
+  mutate(length_cat = ifelse(length > 300, "big", "small")) %>% 
+  filter(scalelength > 1)
